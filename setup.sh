77f06b58d3f468d3e3686a2ecf9e20d12ae91b81
Tomas Kala <me@tomaskala.com>
2021-07-09 11:22:54 +0200

    Add the suckless tools and initializers

---
+++ setup.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+set -euf
+
+
+die() {
+  printf '%s\n' "$1" >&2
+  exit 1
+}
+
+
+if ! command -v make > /dev/null; then
+  die "Make is not installed."
+fi
+
+sudo make -C ./dwm clean install
+sudo make -C ./dmenu clean install
+sudo make -C ./slstatus clean install
+sudo make -C ./st clean install
+sudo make -C ./slock clean install
+
+ln -fs "$(pwd)/xprofile" ~/.config/xprofile
+ln -fs "$(pwd)/xinitrc" ~/.config/xinitrc
