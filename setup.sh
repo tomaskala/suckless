286a337b76ccea6b6238f6669975350e0f1cfd38
Tomas Kala <me@tomaskala.com>
2022-03-30 22:27:38 +0200

    Make the ignore list more robust

--- setup.sh
+++ setup.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 set -eu
 
-GLOBIGNORE=".:..:.git:.gitignore"
-
 die() {
   printf '%s\n' "$1" >&2
   exit 1
@@ -30,6 +28,12 @@ sudo make -C ./st clean install
 sudo make -C ./slock clean install
 
 for dotfiles_source in .*; do
+  if [ "${dotfiles_source}" = '.' ] \
+    || [ "${dotfiles_source}" = '..' ] \
+    || [ "${dotfiles_source#.git}" != "${dotfiles_source}" ]; then
+    continue
+  fi
+
   find "${dotfiles_source}" -type f | sort | while read -r dotfile; do
     install_dotfile "${dotfile}"
   done
